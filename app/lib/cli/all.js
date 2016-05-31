63d8aaef05eb3d5c31d1540f0d95f74a441c1412
André Duarte <mail@andreduarte.net>
2016-05-31 10:52:36 +0100

    Fix "all" command

--- app/lib/cli/all.js
+++ app/lib/cli/all.js
@@ -17,12 +17,11 @@ module.exports.handler = (argv) => {
 
     const projectName = argv._[1];
     const repoPath = argv._[2];
-    const analyzeCount = argv._[2] || 10;
     const projectConfig = getProjectConfig(projectName, argv.projectConfig);
 
     return extract(projectConfig, projectName, repoPath)
     .then(() => prepareJSON(projectConfig, projectName))
-    .then(() => prepareResults(projectConfig, projectName, analyzeCount))
+    .then(() => prepareResults(projectConfig, projectName, repoPath))
     .then(() => process.exit(0));
 };
 
